f34bdec1836fa58316d4fa5252788f6a77377c42
jo-svg <144480253+jo-svg@users.noreply.github.com>
2024-11-14 12:32:01 -0500

    ds

--- app/main.js
+++ app/main.js
@@ -1,1 +1,19 @@
 import "./style.css";
+async function getData(){
+    try {
+        const response = await fetch("https://pokeapi.co/api/v2/pokemon/ditto");
+        if (response.status != 200){
+            throw new Error(response);
+        }
+        else{
+            const data = await response.json();
+            document.querySelector('h1').textContent = data.name
+            console.log(data.name)
+        }
+    } catch (error) {
+        console.log(error);
+        alert("yesh")
+    }
+}
+
+getData();
