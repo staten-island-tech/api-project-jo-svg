44d3c1020f7ebe15c387f2f60fe770c19667c5e8
jo-svg <Josephinel43@nycstudents.net>
2024-12-01 17:08:56 -0500

    uhbib

--- app/main.js
+++ app/main.js
@@ -143,15 +143,59 @@ async function weaponData(title) {
     if (weapon) {
         createOpenCard();
         const skins = weapon.skins;
-        skins.forEach((skin, index) => {
-            //index is the position of the current item in the array (starting at 0)
+        const validSkins = skins.filter(skin => skin && skin.displayIcon);
+        if (validSkins.length === 0) {
+            return; 
+        }
+        const badImages = [
+            'https://media.valorant-api.com/weaponskins/f454efd1-49cb-372f-7096-d394df615308/displayicon.png',
+            'https://media.valorant-api.com/weaponskins/9c134f41-4c29-1bd8-682e-178e7f349c9b/displayicon.png',
+            'https://media.valorant-api.com/weaponskins/5305d9c4-4f46-fbf4-9e9a-dea772c263b5/displayicon.png',
+            'https://media.valorant-api.com/weaponskins/9c808029-48f5-ce89-21c5-88bf4228d2ed/displayicon.png',
+            'https://media.valorant-api.com/weaponskins/27f21d97-4c4b-bd1c-1f08-31830ab0be84/displayicon.png',
+            'https://media.valorant-api.com/weaponskins/fff73ad4-4d46-a6d9-43f1-51b633845434/displayicon.png',
+            'https://media.valorant-api.com/weaponskins/724a7f42-4315-eccf-0e76-77bdd3ec2e09/displayicon.png',
+            'https://media.valorant-api.com/weaponskins/7baa9f5a-4ac3-804a-2e47-4da916de0b79/displayicon.png',
+            'https://media.valorant-api.com/weaponskins/337cb216-4a6e-d85d-88c2-f29ab317784c/displayicon.png',
+            'https://media.valorant-api.com/weaponskins/fff73ad4-4d46-a6d9-43f1-51b633845434/displayicon.png',
+            'https://media.valorant-api.com/weaponskins/337cb216-4a6e-d85d-88c2-f29ab317784c/displayicon.png',
+            'https://media.valorant-api.com/weaponskins/68f2e92e-4c94-8104-ed99-03925bbc71e8/displayicon.png',
+            'https://media.valorant-api.com/weaponskins/acd26127-48ff-8b9e-7ba6-b989af8a4b24/displayicon.png',
+            'https://media.valorant-api.com/weaponskins/ed407bc7-4949-3131-b84f-d6be83b63a15/displayicon.png',
+            'https://media.valorant-api.com/weaponskins/70c97fb2-4d79-d4bb-5173-a1888cd4bfd9/displayicon.png',
+            'https://media.valorant-api.com/weaponskins/7b82c605-44bb-aae9-55bf-a4afef323553/displayicon.png',
+            'https://media.valorant-api.com/weaponskins/f06657f3-48b6-6314-7235-a9a2749df5b9/displayicon.png',
+            'https://media.valorant-api.com/weaponskins/3e3ad47a-4383-73f1-4d92-1693059dae8f/displayicon.png',
+            'https://media.valorant-api.com/weaponskins/24aee897-4cdc-b0fd-e596-1ba90fa6d1b2/displayicon.png',
+            'https://media.valorant-api.com/weaponskins/b4903002-4687-1953-847a-e0b09a2b2726/displayicon.png',
+            'https://media.valorant-api.com/weaponskins/1c63b43b-43c4-04e4-01c9-7aa1bffa5ac1/displayicon.png',
+            'https://media.valorant-api.com/weaponskins/d26ce959-4ed2-7105-8f33-dea48e26de4a/displayicon.png',
+            'https://media.valorant-api.com/weaponskins/1ef6ba68-4dbe-30c7-6bc8-93a6c6f13f04/displayicon.png',
+            'https://media.valorant-api.com/weaponskins/075e4cf0-469b-25dc-8d6f-2995fbb093de/displayicon.png',
+            'https://media.valorant-api.com/weaponskins/48ad078a-4dae-2b85-a945-f4b6d1efecbb/displayicon.png',
+            'https://media.valorant-api.com/weaponskins/acdd29aa-44b5-0d78-dc54-8f92ed555559/displayicon.png',
+            'https://media.valorant-api.com/weaponskins/d1f2920f-469a-3431-ad96-96afbd0017f2/displayicon.png',
+            'https://media.valorant-api.com/weaponskins/2a049f35-4bcd-af25-21fd-ec942e2d5007/displayicon.png',
+            'https://media.valorant-api.com/weaponskins/874025d8-4293-a38a-96ac-3d94954fb4b7/displayicon.png',
+            'https://media.valorant-api.com/weaponskins/7122d78b-4e60-eb4d-5f65-738d7c1ce9ae/displayicon.png',
+            'https://media.valorant-api.com/weaponskins/3bf1e8e0-47e8-f27a-6054-929575f41a54/displayicon.png',
+            'https://media.valorant-api.com/weaponskins/bd9c41dd-42b1-8555-78c1-81a36016bc03/displayicon.png',
+            'https://media.valorant-api.com/weaponskins/457fe5d2-40c8-4490-6834-53a74e709f4a/displayicon.png',
+            'https://media.valorant-api.com/weaponskins/5211efa8-4efd-09bb-6cee-72b86a8a5972/displayicon.png',
+            'https://media.valorant-api.com/weaponskins/fd44b2d5-49ee-77ab-fa56-588f3ac0c268/displayicon.png',
+            'https://media.valorant-api.com/weaponskins/c79318a6-4ffd-a99f-f452-77947640f688/displayicon.png',
+            'https://media.valorant-api.com/weaponskins/f01d1307-4299-42f5-2c5e-7dab7e69ab19/displayicon.png',
+            'https://media.valorant-api.com/weaponskins/d25dfdc4-4fce-e121-88ef-66b5dadeee09/displayicon.png',
+            'https://media.valorant-api.com/weaponskins/940fb417-4a9c-3004-41f5-3e8f1f4178b2/displayicon.png'
+        ]
+        const filteredSkins = validSkins.filter(skin => !badImages.includes(skin.displayIcon));
+        filteredSkins.forEach((skin, index) => {
             const slideId = `slide${index + 1}`;
-            //index + 1 ensures IDs start from slide1 instead of slide0. 
-            const prevSlideId = `slide${index === 0 ? skins.length : index}`;
-            const nextSlideId = `slide${index === skins.length - 1 ? 1 : index + 2}`;
+            const prevSlideId = `slide${index === 0 ? filteredSkins.length : index}`; 
+            const nextSlideId = `slide${index === filteredSkins.length - 1 ? 1 : index + 2}`; 
             createSkinPart(skin, slideId, prevSlideId, nextSlideId);
         });
-        stats(weapon)
+        stats(weapon);
     }
 }
 
@@ -161,20 +205,21 @@ function createOpenCard() {
         <div class="carousel" style="width: 40rem;"></div>
     `);
 }
-function createSkinPart(skin, slideId, prevSlideId, nextSlideId, weapon) {
+function createSkinPart(skin, slideId, prevSlideId, nextSlideId) {
+    if (!skin || !skin.displayIcon) {
+        return; 
+    }
     const carousel = document.querySelector(".carousel");
     const skinIcon = skin.displayIcon || "https://via.placeholder.com/150"; 
-    const backgroundColor = "lightblue"; 
-
     carousel.insertAdjacentHTML("beforeend", `
-        <div id="${slideId}" class="carousel-item relative flex justify-center items-center w-full" style="background-color: ${backgroundColor};">
+        <div id="${slideId}" class="carousel-item relative flex justify-center items-center w-full">
             <div class="flex justify-center items-center h-96">
                 <img
                     src="${skinIcon}"
                     class="object-contain"
                     style="max-width: 80%; max-height: 80%; border-radius: 8px;" 
-                    alt="${skin.displayName}" 
-                />
+                    alt="${skin.displayName}"
+                    />
             </div>
             <div class="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
                 <a href="#${prevSlideId}" class="btn btn-circle">❮</a>
